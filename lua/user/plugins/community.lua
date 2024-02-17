efb9d38cb2b1f691c9f021ed56eb7520a198c394
Jason Yates <yatesj9@gmail.com>
2024-02-16 23:20:46 -0500

    Add user

---
+++ lua/user/plugins/community.lua
@@ -0,0 +1,15 @@
+return {
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+  -- example of importing a plugin, comment out to use it or add your own
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+
+  { import = "astrocommunity.colorscheme.catppuccin"} ,
+  { import = "astrocommunity.colorscheme.nord-nvim"},
+  { import = "astrocommunity.colorscheme.sonokai"},
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim"},
+  { import = "astrocommunity.editing-support.multicursors-nvim"},
+
+
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+}
