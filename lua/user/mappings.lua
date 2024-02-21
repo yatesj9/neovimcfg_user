6842fc1919ef81310bfccddc27219b633da7b1c2
Jason Yates <yatesj9@gmail.com>
2024-02-21 14:53:09 -0500

    Mapping update

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -30,11 +30,15 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+["<leader>a"] = {"<cmd>ToggleTermSendCurrentLine<cr>",desc = "Send to Terminal" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
+  },
+  v = {
+   ["<leader>a"] = {"<cmd>ToggleTermSendVisualLines<cr>",desc = "Send to Terminal" }, 
   },
 }
